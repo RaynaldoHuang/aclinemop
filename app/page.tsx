3cd35d2fdd9af9ad87d2c9875b9cc4601c084805
RaynaldoHuang <huangraynaldo@gmail.com>
2025-01-25 13:16:00 +0700

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,15 @@ import fb7 from "@/public/png/fb7.webp"
 import testi from "@/public/png/testi.png"
 import fb8 from "@/public/png/fb7.jpg"
 import gif from "@/public/png/GIF1.gif"
+import { fbq } from "react-facebook-pixel";
 
 export default function Home() {
+  const handleCheckoutClick = () => {
+    // Menambahkan Facebook Pixel event 'InitiateCheckout'
+    if (typeof fbq !== "undefined") {
+      fbq('track', 'InitiateCheckout');
+    }
+  };
   return (
     <div className="w-full max-w-[425px] mx-auto">
       <header className="text-white text-center flex justify-center py-6 bg-[#ce0000]">
@@ -134,6 +141,7 @@ export default function Home() {
       <div>
         <Link
           href={"https://wa.me/6285360027891"}
+          onClick={handleCheckoutClick}
           className="flex fixed bottom-0 right-0 bg-green-500 text-white p-4 shadow-lg z-50 hover:bg-green-600 transition duration-300 w-full text-center text-2xl font-bold items-center justify-center"
         >
           <Image
