da0f70afd60de055d0d59566fbdb4229cac85483
RaynaldoHuang <huangraynaldo@gmail.com>
2025-01-25 12:41:56 +0700

    asasa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import ClientFbpiksel from "../components/ClientFbpiksel";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
