6624aac2d3d2a2f00471f3773bb4d0517fa3b8a0
RaynaldoHuang <huangraynaldo@gmail.com>
2025-01-25 13:40:58 +0700

    cpek

--- app/facebookpixel.js
+++ app/facebookpixel.js
@@ -1,5 +1,3 @@
-// facebookPixel.js
-
 import { useEffect } from 'react';
 
 const FacebookPixel = () => {
@@ -8,19 +6,28 @@ const FacebookPixel = () => {
         if (typeof window !== 'undefined' && typeof fbq === 'undefined') {
             // Replace 'your-pixel-id' with your actual Facebook Pixel ID
             !function (f, b, e, v, n, t, s) {
-                if (f.fbq) return; n = f.fbq = function () {
-                    n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push
-                        (arguments)
-                }; if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !
-                    0; n.version = '2.0'; n.queue = []; t = b.createElement(e);
-                t.async = !0; t.src = v; s = b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t, s)
-            }(window, document,
-                'script',
-                'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1793750594713308');
-            fbq('track', 'PageView'); // Replace with your actual pixel ID
+                if (f.fbq) return;
+                n = f.fbq = function () {
+                    if (n.callMethod) {
+                        n.callMethod.apply(n, arguments);
+                    } else {
+                        n.queue.push(arguments);
+                    }
+                };
+                if (!f._fbq) f._fbq = n;
+                n.push = n;
+                n.loaded = true;
+                n.version = '2.0';
+                n.queue = [];
+                t = b.createElement(e);
+                t.async = true;
+                t.src = v;
+                s = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s);
+            }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1793750594713308');  // Replace with your actual pixel ID
+            fbq('track', 'PageView');
         }
     }, []);
 
