87d246816623cc112c15c7a097c112b4093f89ca
RaynaldoHuang <huangraynaldo@gmail.com>
2025-01-25 12:39:56 +0700

    errrfiff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ClientFbpiksel from "../components/ClientFbpiksel";
+// import ClientFbpiksel from "../components/ClientFbpiksel";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -58,7 +58,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ClientFbpiksel />
         {children}
       </body>
     </html>
