380790559a2ddfa3f0efc5f38e78b95c02c02bf3
RaynaldoHuang <huangraynaldo@gmail.com>
2025-01-25 13:42:27 +0700

    cpek

--- app/facebookpixel.js
+++ app/facebookpixel.js
@@ -2,30 +2,38 @@ import { useEffect } from 'react';
 
 const FacebookPixel = () => {
     useEffect(() => {
-        // Initialize Facebook Pixel tracking (fbq)
         if (typeof window !== 'undefined' && typeof fbq === 'undefined') {
-            // Replace 'your-pixel-id' with your actual Facebook Pixel ID
-            !function (f, b, e, v, n, t, s) {
-                if (f.fbq) return;
-                n = f.fbq = function () {
+            // Initialize Facebook Pixel tracking (fbq)
+            function initializeFacebookPixel() {
+                if (window.fbq) return; // If fbq already exists, return early
+
+                const n = window.fbq = function () {
                     if (n.callMethod) {
                         n.callMethod.apply(n, arguments);
                     } else {
                         n.queue.push(arguments);
                     }
                 };
-                if (!f._fbq) f._fbq = n;
+
+                if (!window._fbq) window._fbq = n;
+
                 n.push = n;
                 n.loaded = true;
                 n.version = '2.0';
                 n.queue = [];
-                t = b.createElement(e);
-                t.async = true;
-                t.src = v;
-                s = b.getElementsByTagName(e)[0];
-                s.parentNode.insertBefore(t, s);
-            }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
 
+                const t = document.createElement('script');
+                t.async = true;
+                t.src = 'https://connect.facebook.net/en_US/fbevents.js';
+
+                const s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(t, s);
+            }
+
+            // Call the initialization function
+            initializeFacebookPixel();
+
+            // Track the page view
             fbq('init', '1793750594713308');  // Replace with your actual pixel ID
             fbq('track', 'PageView');
         }
