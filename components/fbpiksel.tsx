b4e1746cf1a5288ebcaf1f8358bada0863d64c9d
RaynaldoHuang <huangraynaldo@gmail.com>
2025-01-25 12:37:57 +0700

    asaasa

--- components/fbpiksel.tsx
+++ components/fbpiksel.tsx
@@ -1,31 +1,34 @@
+// components/fbpiksel.js
+"use client";
+
 import { useEffect } from "react";
 import ReactPixel, { fbq } from "react-facebook-pixel";
 
 const PixelTracker = () => {
-    useEffect(() => {
-      const pixelId = "1793750594713308";
-      ReactPixel.init(pixelId);
-      ReactPixel.pageView();
-    }, []);
+  useEffect(() => {
+    const pixelId = "1793750594713308";
+    ReactPixel.init(pixelId);
+    ReactPixel.pageView();
+  }, []);
 
-    const handleWhatsAppClick = () => {
-        if (typeof fbq !== "undefined") {
-          fbq("track", "InitiateCheckout", {
-            content_name: "WhatsApp Order Button",
-            content_category: "Contact",
-            value: 0, // Nilai transaksi jika relevan
-            currency: "IDR", // Mata uang
-          });
-        }
-      };
-    
-      return (
-        <div>
-          <button onClick={handleWhatsAppClick} style={{ display: "none" }}>
-            Track Click
-          </button>
-        </div>
-      );
-    };
-    
-  export default PixelTracker;
+  const handleWhatsAppClick = () => {
+    if (typeof fbq !== "undefined") {
+      fbq("track", "InitiateCheckout", {
+        content_name: "WhatsApp Order Button",
+        content_category: "Contact",
+        value: 0,
+        currency: "IDR",
+      });
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleWhatsAppClick} style={{ display: "none" }}>
+        Track Click
+      </button>
+    </div>
+  );
+};
+
+export default PixelTracker;
