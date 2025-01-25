ebbb009876897f324214a919bb9d340f27afff3c
RaynaldoHuang <huangraynaldo@gmail.com>
2025-01-25 11:36:18 +0700

    fix errr2

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,6 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const Fbpiksel = dynamic(() => import("../components/fbpiksel"), { ssr: false });
-
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -20,6 +17,8 @@ export const metadata: Metadata = {
   title: "ALAT PEL PRAKTIS",
   description: "Solusi inovatif untuk membersihkan rumah Anda dengan lebih efisien dan mudah. Dengan kepala pel yang dapat berputar 360°, alat ini memungkinkan Anda untuk mencapai setiap sudut ruangan, bahkan di bawah tempat-tempat yang sulit dijangkau seperti tempat tidur atau sofa. Menghindari penumpukan debu dan membantu membersihkan rumah Anda tanpa menyisakan kotoran di sudut.",
 };
+
+const Fbpiksel = dynamic(() => import("../components/fbpiksel"), { ssr: false });
 
 export default function RootLayout({
   children,
